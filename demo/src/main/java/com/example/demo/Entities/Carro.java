895e39db9ef093eab4f7fb9c1dafa843ea33cab1
GabrielHenrique-corinthians <gabrielvilasboassilva@gmail.com>
2025-10-04 15:38:13 -0300

    Feat/Criação da entidade Carro e da interface

---
+++ demo/src/main/java/com/example/demo/Entities/Carro.java
@@ -0,0 +1,45 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "carro")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "modelo", nullable = false)
+    private String modelo;
+
+    @Column(name = "marca", nullable = false)
+    private String marca;
+
+    @Column(name = "ano", nullable = false)
+    private Integer ano;
+
+    @Column(name = "placa", nullable = false, unique = true)
+    private String placa;
+
+    @Column(name = "disponivel", nullable = false)
+    private Boolean disponivel;
+
+    @Column(name = "precoDiaria", nullable = false)
+    private BigDecimal precoDiaria;
+
+}
