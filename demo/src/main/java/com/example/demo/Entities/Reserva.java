0b218c958c7769fb0d85b83e395628dac0eb54ba
Joao Victor <vcarneirooo2@gmail.com>
2025-10-07 22:22:12 -0300

    Fix/Excluindo atributo duplicado

--- demo/src/main/java/com/example/demo/Entities/Reserva.java
+++ demo/src/main/java/com/example/demo/Entities/Reserva.java
@@ -1,35 +1,23 @@
 package com.example.demo.Entities;
-    
 
 import java.time.LocalDateTime;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
-@Entity
-@Table(name = "reserva")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "reserva")
 public class Reserva {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-   
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-     @Column(name = "ClienteId", nullable = false)
-    private Long clienteId;
-
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
-      @Column(name = "CarroId", nullable = false)
-    private Long carroId;
 
     @Column(name = "data_inicio", nullable = false)
     private LocalDateTime dataInicio;
@@ -37,15 +25,15 @@ public class Reserva {
     @Column(name = "data_fim", nullable = false)
     private LocalDateTime dataFim;
 
-    @Column(name = "Status",nullable = false)
+    @Column(name = "status", nullable = false) 
     private String status;
 
-    @ManyToOne
-    @JoinColumn(name = "cliente_id")
+    @ManyToOne(fetch = FetchType.LAZY) 
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
 
-    @ManyToOne
-    @JoinColumn(name = "carro_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carro_id", nullable = false)
     private Carro carro;
 
 }
