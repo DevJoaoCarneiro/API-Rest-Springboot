91a6b0ca8bf3f42a92b75bf48516d449e1ed9f73
donatodvdm <donatodevellis240@gmail.com>
2025-10-07 21:47:04 -0300

    Agregado de relacionamentos na entidade reserva

--- demo/src/main/java/com/example/demo/Entities/Reserva.java
+++ demo/src/main/java/com/example/demo/Entities/Reserva.java
@@ -3,12 +3,7 @@ package com.example.demo.Entities;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -40,4 +35,13 @@ public class Reserva {
 
     @Column(name = "Status",nullable = false)
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "carro_id")
+    private Carro carro;
+
 }
